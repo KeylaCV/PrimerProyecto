425b4612afc31a462a4e1fb3ecb54b6277ca4d92
keyli <keyli@Keylita>
2023-04-17 09:22:53 +0200

    Modificado por Keyla

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import Keyla.AppGestion;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo,soy ceinmark");
+		System.out.println("Chanfle");
 	}
 }
